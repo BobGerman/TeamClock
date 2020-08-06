975f350afbb8b782d125cd43d4e59dee74df3f47
Bob German <bgerman1@hotmail.com>
2020-08-05 22:23:21 -0400

    Copied sketch from plunker

--- src/components/TeamClock.tsx
+++ src/components/TeamClock.tsx
@@ -1,16 +1,35 @@
 import * as React from 'react';
-require ('./TeamClock.css');
+require('./TeamClock.css');
 
 export interface ITeamClockProps {
   message: string;
 }
 
 export default class GroupTable extends React.Component<ITeamClockProps, {}> {
-  
+
   public render(): React.ReactElement<ITeamClockProps> {
 
     return (
-      <div>{this.props.message}</div>
-      );
+      <div>
+        <h1>Hello {name}!</h1>
+
+        <div className="worldClock">
+          <div className="timeStrip">
+            <div className="timeCell">
+              <div className="timeHeader">Monday</div>
+              <div className="timeBody day">01 Dec</div>
+            </div>
+            <div className="timeCell">
+              <div className="timeHeader"></div>
+              <div className="timeBody evening">1 am</div>
+            </div>
+            <div className="timeCell">
+              <div className="timeHeader"></div>
+              <div className="timeBody night">2 am</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
