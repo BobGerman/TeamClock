75f8811f17dfaf78ec64f79232d270e5775bd64f
Bob German <bgerman1@hotmail.com>
2020-08-07 17:56:08 -0400

    Added labels for team members

--- TeamClock/src/components/ClockTab.js
+++ TeamClock/src/components/ClockTab.js
@@ -25,12 +25,25 @@ class ClockTab extends React.Component {
 
     // let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
 
+    const now = new Date();
+    const aycaTime = new Date().getTime() + (6 * 60 * 60 * 1000);
+    const barnamTime = new Date().getTime() + (14 * 60 * 60 * 1000);
+    const bobTime = new Date().getTime() + (-4 * 60 * 60 * 1000);
+    const toddTime = new Date().getTime() + (-5 * 60 * 60 * 1000);
     return (
       <div>
         <div className="teamClock">
+          <div className="personList">
+            <div className="person">Ayca</div>
+            <div className="person">Barnam</div>
+            <div className="person">Bob</div>
+            <div className="person">Todd</div>
+          </div>
           <div className="scrollingWrapper">
-            <TimeStrip hours="24" startDate={Date.now()} />
-            <TimeStrip hours="24" startDate={Date.now()} />
+            <TimeStrip hours="24" startDate={aycaTime} />
+            <TimeStrip hours="24" startDate={barnamTime} />
+            <TimeStrip hours="24" startDate={bobTime} />
+            <TimeStrip hours="24" startDate={toddTime} />
           </div>
         </div>
       </div>
