4954f5e28df25d9d48d85e85980113283263e487
Bob German <bgerman1@hotmail.com>
2020-08-07 18:27:11 -0400

    Now uses mock data, but time is wrong

--- TeamClock/src/components/ClockTab.js
+++ TeamClock/src/components/ClockTab.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './ClockTab.css';
 import TimeStrip from './TimeStrip';
 import * as microsoftTeams from "@microsoft/teams-js";
+import TeamService from '../services/TeamService';
 
 class ClockTab extends React.Component {
   constructor(props) {
@@ -25,25 +26,21 @@ class ClockTab extends React.Component {
 
     // let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
 
-    const now = new Date();
-    const aycaTime = new Date().getTime() + (6 * 60 * 60 * 1000);
-    const barnamTime = new Date().getTime() + (14 * 60 * 60 * 1000);
-    const bobTime = new Date().getTime() + (-4 * 60 * 60 * 1000);
-    const toddTime = new Date().getTime() + (-5 * 60 * 60 * 1000);
+    const teamMembers = (new TeamService).getTeamMembers();
+
     return (
       <div>
         <div className="teamClock">
           <div className="personList">
-            <div className="person">Ayca</div>
-            <div className="person">Barnam</div>
-            <div className="person">Bob</div>
-            <div className="person">Todd</div>
+            {teamMembers.map((m) => {
+              return <div className="person">{m.name}</div>;
+            }) }
           </div>
           <div className="scrollingWrapper">
-            <TimeStrip hours="24" startDate={aycaTime} />
-            <TimeStrip hours="24" startDate={barnamTime} />
-            <TimeStrip hours="24" startDate={bobTime} />
-            <TimeStrip hours="24" startDate={toddTime} />
+            {teamMembers.map((m) => {
+                const t = new Date().getTime() + (m.utcOffset * 60 * 60 * 1000);
+                return <TimeStrip hours="24" startDate={t} />;
+              }) }
           </div>
         </div>
       </div>
---
+++ TeamClock/src/services/TeamService.js
@@ -0,0 +1,48 @@
+export default class TeamService{
+
+    getTeamMembers () {
+        return [
+            {
+                name: 'Ayca',
+                city: 'Dubai',
+                utcOffset: 6,
+            },
+            {
+                name: 'Barnam',
+                city: 'Melbourne',
+                utcOffset: 14,
+            },
+            {
+                name: 'Bob',
+                city: 'Boston',
+                utcOffset: -4,
+            },
+            {
+                name: 'Dan',
+                city: 'Phoenix',
+                utcOffset: -6,
+            },
+            {
+                name: 'Emily',
+                city: 'Bay area',
+                utcOffset: -7,
+            },
+            {
+                name: 'Matt',
+                city: 'Bay area',
+                utcOffset: -7,
+            },
+            {
+                name: 'Todd',
+                city: 'Austin',
+                utcOffset: -5,
+            },
+            {
+                name: 'Tomomi',
+                city: 'Bay area',
+                utcOffset: -7,
+            },
+        ]
+    }
+
+}
