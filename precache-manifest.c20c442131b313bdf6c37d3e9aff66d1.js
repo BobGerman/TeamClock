b18d17ef1fcc54bc729552e8acc4f50bde6fb436
Bob German <bgerman1@hotmail.com>
2020-08-09 16:26:56 -0400

    Updates

---
+++ precache-manifest.c20c442131b313bdf6c37d3e9aff66d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39b40dcec70a4aa0a9d1db406df5663",
+    "url": "/TeamClock/index.html"
+  },
+  {
+    "revision": "bf56d6f5f790da91c43d",
+    "url": "/TeamClock/static/css/main.36fc554f.chunk.css"
+  },
+  {
+    "revision": "18eb8f9b4aefeb1c33b2",
+    "url": "/TeamClock/static/js/2.1a93bd27.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/TeamClock/static/js/2.1a93bd27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf56d6f5f790da91c43d",
+    "url": "/TeamClock/static/js/main.10a02167.chunk.js"
+  },
+  {
+    "revision": "6598b093112827515ebd",
+    "url": "/TeamClock/static/js/runtime-main.61b109e6.js"
+  }
+]);
