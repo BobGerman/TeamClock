3e3aca741ab556aed84cca38dfa20fa0110ffad3
Bob German <bgerman1@hotmail.com>
2020-08-09 17:54:39 -0400

    Fixed tab config page

--- TeamClock/src/components/TabConfig.js
+++ TeamClock/src/components/TabConfig.js
@@ -19,7 +19,8 @@ class TabConfig extends React.Component {
        * This allows for the addition of query string parameters based on
        * the settings selected by the user.
        */
-      microsoftTeams.settings.setSettings({"contentUrl": "https://localhost:3000/tab"});
+      const baseUrl = window.location.href.substring(0,window.location.href.indexOf("#/")+1);
+      microsoftTeams.settings.setSettings({"contentUrl": `${baseUrl}/tab`});
   
       /**
        * After verifying that the settings for your tab are correctly
