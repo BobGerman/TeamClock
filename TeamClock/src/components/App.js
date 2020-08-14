d3a6844a0e37ab2d122433f077018e13b6c917f5
Bob German <bgerman1@hotmail.com>
2020-08-14 19:10:46 -0400

    Removed TeamsHostError component to its own file

--- TeamClock/src/components/App.js
+++ TeamClock/src/components/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import './App.css';
-import * as microsoftTeams from "@microsoft/teams-js";
 import { HashRouter as Router, Route } from "react-router-dom";
+import * as microsoftTeams from "@microsoft/teams-js";
+import AuthService from '../services/AuthService'
+import './App.css';
+
+// React components
 import Privacy from "./Privacy";
 import TermsOfUse from "./TermsOfUse";
 import Tab from './Tab';
+import TeamsHostError from './TeamsHostError';
 import TabConfig from './TabConfig';
 import Web from './Web';
-import AuthService from '../services/AuthService'
 
-/**
- * The main app which handles the initialization and routing
- * of the app.
- */
 class App extends React.Component {
 
   constructor() {
@@ -34,12 +33,13 @@ class App extends React.Component {
   }
 
   render() {
+
     if (!this.state.authInitialized) {
 
       // Wait for Auth Service to initialize
       return (<p>Loading...</p>);
 
-    } else if (microsoftTeams) {
+    } else {
 
       if (window.parent === window.self) {
 
@@ -56,43 +56,22 @@ class App extends React.Component {
             </Router>
           </div>
         );
+      } else {
+
+        // If here we are in an IFrame - assume we are in Teams and initialize
+        microsoftTeams.initialize(window);
+
+        // Only a couple of the pages in the app work in Teams tabs
+        return (
+          <div className="App">
+            <Router>
+              <Route exact path="/tab" component={Tab} />
+              <Route exact path="/config" component={TabConfig} />
+            </Router>
+          </div>
+        );
       }
-
-      // If here we are in an IFrame - assume we are in Teams and initialize
-      microsoftTeams.initialize(window);
-
-      // Only a couple of the pages in the app work in Teams tabs
-      return (
-        <div className="App">
-          <Router>
-            <Route exact path="/tab" component={Tab} />
-            <Route exact path="/config" component={TabConfig} />
-          </Router>
-        </div>
-      );
-    } else {
-
-      // If here, the Teams SDK is missing in action
-      return (
-        <h3>Microsoft Teams SDK not found.</h3>
-      );
-
     }
-
-  }
-}
-
-/**
- * This component displays an error message in the
- * case when a page is not being hosted within Teams.
- */
-class TeamsHostError extends React.Component {
-  render() {
-    return (
-      <div>
-        <h3 className="Error">Debug your app within the Teams client.</h3>
-      </div>
-    );
   }
 }
 
