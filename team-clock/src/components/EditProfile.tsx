4bdcaab57067ddda2f4c4c6ca3bc8e08009226ad
Bob German <bgerman1@hotmail.com>
2021-04-16 12:31:58 -0400

    Set up task module but list fails to render

--- team-clock/src/components/EditProfile.tsx
+++ team-clock/src/components/EditProfile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ITeamService from '../services/TeamService/ITeamService';
+import * as microsoftTeams from "@microsoft/teams-js";
 
 export interface IEditProfileProps {
     teamService: ITeamService;
@@ -7,12 +8,35 @@ export interface IEditProfileProps {
 
 export default class EditProfile extends React.Component<IEditProfileProps> {
 
-    public render() {
-        const editUrl = this.props.teamService.getEditUrl();
+    public componentDidMount() {
+        if (microsoftTeams) {
+            microsoftTeams.initialize();
+        }
+    }
 
+    public render() {
         return <div>
-            <a href={editUrl} target="_blank">Edit My Profile</a>
+            <button onClick={this.launchEditor.bind(this)}>Edit My Profile</button>
         </div>;
+    }
+
+    private launchEditor(event: React.MouseEvent<HTMLButtonElement>) {
+        const url = this.props.teamService.getEditUrl();
+        if (microsoftTeams) {
+            const taskModuleInfo = {
+                title: "Editor",
+                url: url,
+                // Show it as large as Teams will allow
+                width: Number.MAX_VALUE,
+                height: Number.MAX_VALUE
+            };
+            microsoftTeams.tasks.startTask(taskModuleInfo,
+                (() => { this.refresh(); }));
+        }
+    }
+
+    private refresh() {
+        // this.props.refresh();
     }
 
 }
