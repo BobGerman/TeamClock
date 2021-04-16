93b6ccc145f52e12aba30272fa01c75c5b23c50b
Bob German <bgerman1@hotmail.com>
2021-04-16 10:44:05 -0400

    TeamClock now handles popup error

--- team-clock/src/components/TeamClock.tsx
+++ team-clock/src/components/TeamClock.tsx
@@ -11,46 +11,57 @@ import IPerson from '../model/IPerson';
 export interface ITeamClockProps {
   teamService: ITeamService;
   clockService: IClockService;
- };
+};
+enum loadingState { waiting, failed, success }
 export interface ITeamClockState {
   currentUser?: IPerson;
   teamMembers: IPerson[];
   timeZones: ITimeZone[];
   participants: IPerson[];
+  dataLoaded: loadingState;
 };
 
 /**
  * The web UI used when Teams pops out a browser window
  */
-export default class TeamClock extends React.Component <ITeamClockProps, ITeamClockState> {
+export default class TeamClock extends React.Component<ITeamClockProps, ITeamClockState> {
   constructor(props: ITeamClockProps) {
     super(props);
     this.state = {
       currentUser: undefined,
       teamMembers: [],
       timeZones: [],
-      participants: []
+      participants: [],
+      dataLoaded: loadingState.waiting
     };
   }
 
   componentDidMount() {
     if (this.props.teamService && this.props.clockService) {
-      this.loadData(this.props.teamService, this.props.clockService);
+      this.loadData(this.props.teamService, this.props.clockService, true);
     }
   }
 
   // Right now this is called only by componentDidMount()
   // Some day an event handler will call this to reload data after a user
   // makes some changes
-  async loadData(teamService: ITeamService, clockService: IClockService) {
-    const currentUser = await teamService.getCurrentUser("WHAT IS THIS");
-    const teamMembers = await teamService.getOtherTeamMembers(currentUser);
-    const timeZones = clockService.getTimeZones(teamMembers);
-    this.setState({
-      currentUser: currentUser,
-      teamMembers: teamMembers,
-      timeZones: timeZones
-    })
+  async loadData(teamService: ITeamService, clockService: IClockService, failSilently: boolean) {
+    try {
+      const currentUser = await teamService.getCurrentUser("WHAT IS THIS");
+      const teamMembers = await teamService.getOtherTeamMembers(currentUser);
+      const timeZones = clockService.getTimeZones(teamMembers);
+      this.setState({
+        currentUser: currentUser,
+        teamMembers: teamMembers,
+        timeZones: timeZones,
+        dataLoaded: loadingState.success
+      });
+    } catch (error) {
+      this.setState({ dataLoaded: loadingState.failed });
+      if (!failSilently) {
+        throw error;
+      }
+    }
   }
 
   public _addParticipant(participant: IPerson) {
@@ -79,9 +90,20 @@ export default class TeamClock extends React.Component <ITeamClockProps, ITeamCl
   }
 
   render() {
-    if (this.props.clockService &&
-        this.state.currentUser &&
-        this.state.timeZones) {
+    if (this.state.dataLoaded === loadingState.waiting) {
+      // Nothing to see here folks
+      return (<div>Loading...</div>);
+    } else if (this.state.dataLoaded === loadingState.failed) {
+      // Failure is usually pop-up blocker; try a button
+      return (<button onClick={async () => {
+        if (this.props.teamService && this.props.clockService) {
+          await this.loadData(this.props.teamService, this.props.clockService, false);
+        }
+      }}>Log in</button>);
+    } else if (this.props.clockService &&
+      this.state.currentUser &&
+      this.state.timeZones) {
+      // If here, data is loaded, display it
       return (
         <main className='teamClock'>
           <header>
@@ -92,28 +114,29 @@ export default class TeamClock extends React.Component <ITeamClockProps, ITeamCl
 
             <div className='currentUser'>
               <DigitalClock clockService={this.props.clockService}
-                            showPhoto={true}
-                            timeZoneObj={this.state.currentUser.timeZoneObj}
-                            user={this.state.currentUser}
-                            timeFormat={this.state.currentUser.timeFormat}
-                            currentUser={true} 
-                            addParticipant={ this._addParticipant.bind(this) }
-                            participants={this.state.participants} />
+                showPhoto={true}
+                timeZoneObj={this.state.currentUser.timeZoneObj}
+                user={this.state.currentUser}
+                timeFormat={this.state.currentUser.timeFormat}
+                currentUser={true}
+                addParticipant={this._addParticipant.bind(this)}
+                participants={this.state.participants} />
             </div>
             <div className="otherTeamMembers">
               <SlideShow slides={this.state.timeZones}
-                         clockService={this.props.clockService} 
-                         showPhoto={true} 
-                         user={this.state.currentUser} 
-                         timeFormat={this.state.currentUser.timeFormat}
-                         addParticipant={ this._addParticipant.bind(this) }
-                         participants={this.state.participants} />
+                clockService={this.props.clockService}
+                showPhoto={true}
+                user={this.state.currentUser}
+                timeFormat={this.state.currentUser.timeFormat}
+                addParticipant={this._addParticipant.bind(this)}
+                participants={this.state.participants} />
             </div>
           </section>
         </main>
       );
     } else {
-      return false;
+      // If here, data loaded successfully but something is missing
+      throw "User login or data access failure";
     }
 
   }
